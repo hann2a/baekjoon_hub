7d063f3f11d96bdd6288cfc517d6a9aa3d5c7517
김소희 <hkanhanea@gmail.com>
2025-09-02 11:16:30 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 226 ms, Memory: 66,304 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.py
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.py
@@ -1,42 +1,32 @@
-# import sys
-# sys.stdin = open('input.txt', 'r')
+def dfs(index, number, add, sub, mul, div):
+    global min_result
+    global max_result
+    
+    # 종료 조건 
+    if index == N:
+        if number < min_result:
+            min_result = number
+        if number > max_result:
+            max_result = number
+        return   # 종료 후 더 내려가지 않도록 return 필요
 
-def dfs(i, current):
-    global max_val, min_val, operation, nums, N
-    if i == N:
-        if current > max_val: max_val = current
-        if current < min_val: min_val = current
-        return
+    if add:
+        dfs(index+1, number+numbers[index], add-1, sub, mul, div)
+    if sub:
+        dfs(index+1, number-numbers[index], add, sub-1, mul, div)
+    if mul:
+        dfs(index+1, number*numbers[index], add, sub, mul-1, div)
+    if div:
+        dfs(index+1, int(number/numbers[index]), add, sub, mul, div-1)
 
-    x = nums[i]
-
-    if operation[0] > 0:   # +
-        operation[0] -= 1
-        dfs(i + 1, current + x)
-        operation[0] += 1
-
-    if operation[1] > 0:   # -
-        operation[1] -= 1
-        dfs(i + 1, current - x)
-        operation[1] += 1
-
-    if operation[2] > 0:   # *
-        operation[2] -= 1
-        dfs(i + 1, current * x)
-        operation[2] += 1
-
-    if operation[3] > 0 and x != 0:  # / (0쪽 절사)
-        operation[3] -= 1
-        dfs(i + 1, int(current / x))   # int()로 0쪽 절사
-        operation[3] += 1
 
 T = int(input())
-for tc in range(1, T + 1):
+for t in range(1, T+1):
     N = int(input())
-    operation = list(map(int, input().split()))  # [+, -, *, /]
-    nums = list(map(int, input().split()))
-    max_val = -10**18
-    min_val =  10**18
-
-    dfs(1, nums[0])
-    print(f"#{tc} {max_val - min_val}")
+    add, sub, mul, div = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    
+    min_result = int(1e8)
+    max_result = int(-1e8)
+    dfs(1, numbers[0], add, sub, mul, div)
+    print(f'#{t} {max_result-min_result}')
